18dd23bb670cea3051aa6c23be5227743ac72902
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-19 14:46:10 +0100

    Upload nft-indexer

---
+++ Week 6 - Solidity Core/nft-indexer/README.md
@@ -0,0 +1,12 @@
+# Simple NFT Indexer 🗄️
+
+👋 Welcome to the NFT Indexer!
+🔍 This tool allows you to easily find all ERC-721 tokens in a given wallet address.
+💰 Simply input the wallet address and let the tool do the rest.
+🚀 Happy indexing!
+
+## Set Up
+
+1. Install dependencies by running `npm install`
+2. Add your API key in the App.jsx file `<-- ALCHEMY API KEY -->`
+3. Start application by running `npm run dev`
---
+++ Week 6 - Solidity Core/nft-indexer/src/App.jsx
@@ -0,0 +1,109 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useState } from 'react';
+
+function App() {
+  const [userAddress, setUserAddress] = useState('');
+  const [results, setResults] = useState([]);
+  const [hasQueried, setHasQueried] = useState(false);
+  const [tokenDataObjects, setTokenDataObjects] = useState([]);
+
+  async function getNFTsForOwner() {
+    const config = {
+      apiKey: '<-- ALCHEMY API KEY -->',
+      network: Network.ETH_MAINNET,
+    };
+
+    const alchemy = new Alchemy(config);
+    const data = await alchemy.nft.getNftsForOwner(userAddress);
+    setResults(data);
+
+    const tokenDataPromises = [];
+
+    for (let i = 0; i < data.ownedNfts.length; i++) {
+      const tokenData = alchemy.nft.getNftMetadata(
+        data.ownedNfts[i].contract.address,
+        data.ownedNfts[i].tokenId
+      );
+      tokenDataPromises.push(tokenData);
+    }
+
+    setTokenDataObjects(await Promise.all(tokenDataPromises));
+    setHasQueried(true);
+  }
+  return (
+    <Box w="100vw">
+      <Center>
+        <Flex
+          alignItems={'center'}
+          justifyContent="center"
+          flexDirection={'column'}
+        >
+          <Heading mb={0} fontSize={36}>
+            NFT Indexer 🧪
+          </Heading>
+          <Text>
+            Plug in an address and this website will return all of its NFTs!
+          </Text>
+        </Flex>
+      </Center>
+      <Flex
+        w="100%"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent={'center'}
+      >
+        <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
+        <Input
+          onChange={(e) => setUserAddress(e.target.value)}
+          color="black"
+          w="600px"
+          textAlign="center"
+          p={4}
+          bgColor="white"
+          fontSize={24}
+        />
+        <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="white">
+          Fetch NFTs
+        </Button>
+
+        <Heading my={36}>Here are your NFTs:</Heading>
+
+        {hasQueried ? (
+          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+            {results.ownedNfts.map((e, i) => {
+              return (
+                <Flex
+                  flexDir={'column'}
+                  color="black"
+                  w={'20vw'}
+                  key={e.id}
+                  className="glass"
+                >
+                  <Box>
+                    <b>Name:</b> {tokenDataObjects[i].title}&nbsp;
+                  </Box>
+                  <Image src={tokenDataObjects[i].rawMetadata.image} />
+                </Flex>
+              );
+            })}
+          </SimpleGrid>
+        ) : (
+          'Please make a query! The query may take a few seconds...'
+        )}
+      </Flex>
+    </Box>
+  );
+}
+
+export default App;
